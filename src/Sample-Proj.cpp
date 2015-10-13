2dfcbf742474f37923d464ff5c6587c098ad22b9
faizelkb <faizel.kb@vvdntech.com>
2015-10-13 06:42:16 +0530

    first commit

---
+++ src/Sample-Proj.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Sample-Proj.cpp
+// Author      : FaizelKB
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
